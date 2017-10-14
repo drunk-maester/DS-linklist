21bece9046293d1cd8edf30df96fb9d1f285f106
Sahil Sharma <sahilsharma.nyd@gmail.com>
2017-10-14 23:28:17 +0530

    added a find function
    a find function added to the linklist class that tells whether an element in present in a link list or not

---
+++ +find_function.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+struct node 
+{
+	int data;
+	struct node *next; 
+};
+class linklist
+{
+	node *head,*tail;
+public:
+	linklist()
+	{
+		head =NULL;
+		tail=NULL;
+	}
+	//~linklist();
+	void addnode(int a)
+	{
+		node *temp=new node();
+		temp->data=a;
+		temp->next=NULL;
+		if(head==NULL)
+		{
+			head=temp;
+			tail=temp;
+		}
+		else
+		{
+			tail->next=temp;
+			tail=tail->next;
+		}
+			
+
+
+		cout<<"Data inserted to the link list "<<a<<endl;
+			}
+
+	int length()
+	{
+		int count=0;
+		node* temp;
+		temp=head;
+		while(temp!=NULL)
+		{
+			count++;
+			temp=temp->next;
+		}
+		return count;
+	}
+
+	void findthis(int x)
+	{	bool flag=false;
+		node*temp=head;
+		while(temp!=NULL)
+		{
+			if(temp->data==x)
+				{flag=true; break;}
+			temp=temp->next;
+		}
+	
+	if(flag)
+	cout<<"element found in the link list"; 
+	else	
+ 	cout<<"no such element in linklist";
+	cout<<endl;
+	}
+};
+int main(int argc, char const *argv[])
+{
+	linklist ll;
+	for (int i = 1; i <=10; ++i)
+	{
+		ll.addnode(i);
+	}
+	cout<<"***************************  lenght of linklist  ********************************"<<endl;
+	cout<<"length of linklist   :: "<<ll.length()<<endl;
+	cout<<"*********************  finding an element in linklist  ********************************"<<endl;
+	cout<<"enter the  element you are looing for "<<endl;
+	int x;
+	cin>>x;
+	ll.findthis(x);
+	return 0;
+}
